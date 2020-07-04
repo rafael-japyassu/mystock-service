dca6f45c7c9153ca3c1811ec6b9da15c84193128
Rafael Vasconcelos <rafaelvasconcelos@MacBook-Pro-de-Rafael.local>
2020-07-04 18:09:31 -0300

    Final settings in project

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
 import { LoggerDescription } from '../enums/enums'
